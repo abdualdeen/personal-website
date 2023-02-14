da067cfab6c4f12a6c285cfe0a707c17200d4069
ahamad2 <abdullah.hamad@huskers.unl.edu>
2023-02-13 22:58:37 -0600

    added new logo, github logo and changed it so the new logo displays

--- personal-wesbite/src/components/NavBar.tsx
+++ personal-wesbite/src/components/NavBar.tsx
@@ -1,13 +1,17 @@
+import websiteLogo from '../../public/website-logo.svg';
 const NavBar = () => {
     return (
         <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Abdullah Hamad's Website</a>
+                <a className="navbar-brand" href="#">
+                    <img src="../../public/website-logo.svg" alt="Logo" width="64" height="64" className="d-inline-block align-text-top" />
+                    Abdullah Hamad's Website
+                </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav ms-auto">
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
