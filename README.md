96d194aa323059be89e9b5d50b54abe9ec4ffcef
ahamad2 <abdullah.hamad@huskers.unl.edu>
2023-02-13 23:34:34 -0600

    updated readme and added github link

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # personal-website
+
+### Quick Start
+
+cd to the inner personal-website folder, then run:
+
+```
+npm i
+```
+
+and to start the server,
+
+```
+npm run dev
+```
--- personal-wesbite/src/components/NavBar.tsx
+++ personal-wesbite/src/components/NavBar.tsx
@@ -1,10 +1,9 @@
-import websiteLogo from '../../public/website-logo.svg';
 const NavBar = () => {
     return (
         <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">
-                    <img src="../../public/website-logo.svg" alt="Logo" width="64" height="64" className="d-inline-block align-text-top" />
+                    <img src="../../public/website-logo.svg" alt="Logo" width="64" className="d-inline-block align-text-top" />
                     Abdullah Hamad's Website
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,6 +11,12 @@ const NavBar = () => {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="https://github.com/ahamad2">
+                                <img src="../../public/github-mark.svg" alt="Logo" width="24" className="d-inline-block align-text-top" />
+                                &ensp;My Github
+                            </a>
+                        </li>
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
