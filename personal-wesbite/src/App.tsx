90985905e24b0d87ebef8022245077450acafa7a
ahamad2 <abdullah.hamad@huskers.unl.edu>
2023-02-15 22:29:28 -0600

    cleaned up css and fixed issue with background color

--- personal-wesbite/src/App.tsx
+++ personal-wesbite/src/App.tsx
@@ -5,8 +5,13 @@ function App() {
 
   return (
     <div className="App">
-      <NavBar/>
-      <div className='container'>
+      <NavBar />
+      <div className='container h-100 d-flex justify-content-center align-items-center dark-container'>
+        <div className='main-text'>
+          <h1>Hi! Nice to meet you!</h1>
+          <h2>Abdullah Hamad at your service.</h2>
+          <h3>I am a software engineer with a passion for building software!</h3>
+        </div>
       </div>
     </div>
   )
