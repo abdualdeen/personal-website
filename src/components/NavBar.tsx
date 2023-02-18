27e71aad87611c0e24fa8f97ae422f44959c13ea
ahamad2 <abdullah.hamad@huskers.unl.edu>
2023-02-17 19:01:10 -0600

    cleaned up some unneccessary files

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,8 +3,8 @@ const NavBar = () => {
         <nav className="navbar navbar-dark fixed-top navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">
-                    <img src="/website-logo.svg" alt="Logo" width="48" className="d-inline-block align-text-top" />
-                    Abdullah Hamad's Website
+                    <img src="/website-logo.svg" alt="Logo" width="32" className="d-inline-block align-text-top" />
+                    &ensp;Abdullah Hamad
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
