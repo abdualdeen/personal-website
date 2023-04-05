c3951d9ae6fed0f4c06c50220dc30abe1bded4d4
ahamad2 <abdullah.hamad@huskers.unl.edu>
2023-04-04 23:23:45 -0500

    * added letter component * added scrolling hello component * added programming list icon component to display familar languages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import NavBar from "./components/NavBar";
 import "./App.css";
+import Letter from "./components/Letter";
+import ScrollingHello from "./components/ScrollingHello";
+import ProgrammingIconsList from "./components/ProgrammingIconsList";
 
 // The app is also the home page. I could have added a separate home page but I felt that would clutter the project and add yet another layer that is not needed.
 // There is the index.html, then the main.tsx, and app.tsx. Enough clutter.
@@ -7,14 +10,26 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <div className="flex justify-center mt-20"> {/* this div exists so the text div below can be centered. */}
-        <div className="inline-block rounded-md bg-neutral-700 text-center text-xl text-white px-3 py-3">
-          <h1>Hi! Nice to meet you!</h1>
-          <h2>Abdullah Hamad at your service.</h2>
-          <h3>
-            I am a software engineer with a passion for building software!
-          </h3>
+      <div className="mt-24 flex justify-center">
+        {/* the div above exists so the text div below can be centered. */}
+        <div className="inline-block rounded-md px-3 py-3 text-center text-xl text-white">
+          <ScrollingHello />
+          <h2 className="text-yellow-300">Abdullah Hamad</h2>
+          <h3 className="text-neutral-500">A software engineer with a passion for building software!</h3>
+          <div className="mt-20">
+            <h4>
+              Here are programming languages or libraries I am familiar with
+            </h4>
+            <div className="rounded-md bg-neutral-700">
+              <ProgrammingIconsList />
+            </div>
+          </div>
         </div>
+      </div>
+
+      <div className="mt-20 flex justify-center">
+        <h2 className="">Wanna get in touch? Checkout my resume or LinkedIn!</h2>
+        {/* <Letter /> */}
       </div>
     </div>
   );
---
+++ src/components/ProgrammingIconsList.tsx
@@ -0,0 +1,116 @@
+import React from "react";
+import "./programmingIconsList.css";
+
+function ProgrammingIconsList() {
+  return (
+    <div className="container">
+      <a href="https://en.wikipedia.org/wiki/Python_(programming_language)">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original-wordmark.svg"
+          alt="Python"
+          className="icon"
+        />
+      </a>
+      <a href="https://www.php.net/">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg"
+          alt="PHP"
+          className="icon"
+        />
+      </a>
+      <a href="https://phalcon.io/en-us">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/phalcon/phalcon-original.svg"
+          alt="Phalcon"
+          className="icon"
+        />
+      </a>
+      <a href="https://www.javascript.com/">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+          alt="JavaScript"
+          className="icon"
+        />
+      </a>
+      <a href="https://www.typescriptlang.org/">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
+          alt="TypeScript"
+          className="icon"
+        />
+      </a>
+      <a href="https://www.w3.org/Style/CSS/Overview.en.html">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+          alt="CSS"
+          className="icon"
+        />
+      </a>
+      <a href="https://getbootstrap.com/">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg"
+          alt="Bootstrap"
+          className="icon"
+        />
+      </a>
+      <a href="https://tailwindcss.com/">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-plain.svg"
+          alt="TailwindCSS"
+          className="icon"
+        />
+      </a>
+      <a href="https://developer.mozilla.org/en-US/docs/Web/HTML">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+          alt="HTML"
+          className="icon"
+        />
+      </a>
+      <a href="https://jquery.com/">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/jquery/jquery-original-wordmark.svg"
+          alt="HTML"
+          className="icon"
+        />
+      </a>
+      <a href="https://www.mysql.com/">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
+          alt="MySQL"
+          className="icon"
+        />
+      </a>
+      <a href="https://www.oracle.com/java/technologies/">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original-wordmark.svg"
+          alt="Java"
+          className="icon"
+        />
+      </a>
+      <a href="https://en.wikipedia.org/wiki/C_(programming_language)">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg"
+          alt="C"
+          className="icon"
+        />
+      </a>
+      <a href="https://en.wikipedia.org/wiki/C++">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg"
+          alt="C++"
+          className="icon"
+        />
+      </a>
+      <a href="https://en.wikipedia.org/wiki/Assembly_language">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/e/ef/APNG_Assembler_Logo.svg"
+          alt="Assembly"
+          className="icon"
+        />
+      </a>
+    </div>
+  );
+}
+
+export default ProgrammingIconsList;
