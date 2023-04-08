6ac5a8548d7759d546d7fbea42f933bbec932708
ahamad2 <abdullah.hamad@huskers.unl.edu>
2023-04-08 02:04:32 -0500

    solved issue with firebase loading and not the local dev server not loading app

--- src/main.tsx
+++ src/main.tsx
@@ -4,23 +4,42 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Charts from "./pages/Charts";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBU1fjZ3fMLKryudRPULeU3wxUtiUKb_K4",
-  authDomain: "abdullahhamad-ef3e2.firebaseapp.com",
-  projectId: "abdullahhamad-ef3e2",
-  storageBucket: "abdullahhamad-ef3e2.appspot.com",
-  messagingSenderId: "659824625343",
-  appId: "1:659824625343:web:2f06b611776f5a7df6e80a",
-  measurementId: "G-CTHC8X3K61",
-};
+/**
+ * Initializing Firebase
+ * the import and init is done this way to allow for local development without loading in Firebase.
+ */
+if (!window.location.origin.includes("localhost:5173")) {
+  import("firebase/app")
+    .then((firebase) => {
+      const firebaseConfig = {
+        apiKey: "AIzaSyBU1fjZ3fMLKryudRPULeU3wxUtiUKb_K4",
+        authDomain: "abdullahhamad-ef3e2.firebaseapp.com",
+        projectId: "abdullahhamad-ef3e2",
+        storageBucket: "abdullahhamad-ef3e2.appspot.com",
+        messagingSenderId: "659824625343",
+        appId: "1:659824625343:web:2f06b611776f5a7df6e80a",
+        measurementId: "G-CTHC8X3K61",
+      };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+      const { initializeApp } = firebase.default;
+      import("firebase/analytics")
+        .then((analytics) => {
+          const { getAnalytics } = analytics.default;
+
+          // Initialize Firebase
+          const firebaseApp = initializeApp(firebaseConfig);
+          const analyticsInstance = getAnalytics(firebaseApp);
+        })
+        .catch((error) => {
+          console.error("Failed to load firebase/analytics:", error);
+        });
+    })
+    .catch((error) => {
+      console.error("Failed to load firebase/app:", error);
+    });
+}
+
 
 const router = createBrowserRouter([
   {
@@ -29,8 +48,8 @@ const router = createBrowserRouter([
   },
   {
     path: "charts",
-    element: <Charts />
-  }
+    element: <Charts />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
